ff32542eb4ac29c8504564e6752172c0d5dc7320
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-05-25 00:57:40 +0600

    my order get api complete

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors())
 app.use(express.json())
 require('dotenv').config()
@@ -20,10 +20,20 @@ const run = async () => {
         const helmetCollection = client.db('Bike_Parts').collection('helmet')
         const partsCollection = client.db('Bike_Parts').collection('parts')
         const reviewCollection = client.db('Bike_Parts').collection('review')
+        const usersOrderCollection = client.db('Bike_Parts').collection('users_order')
         // get service data 
         app.get('/service-get', async (req, res) => {
             const result = await serviceCollection.find().toArray()
             res.send(result)
+        })
+        // get service by id 
+        app.get('/get-service/:id',async(req,res)=>{
+            const id=req.params.id
+            console.log(id)
+            const filter={_id:ObjectId(id)}
+            const result=await serviceCollection.findOne(filter)
+            res.send(result)
+
         })
         // get helmet data 
         app.get('/get-helmet', async (req, res) => {
@@ -36,12 +46,26 @@ const run = async () => {
             res.send(result)
 
         })
-        // get all review
+        // get all review   
         app.get('/get-review', async (req, res) => {
             const result = await reviewCollection.find().toArray()
             res.send(result)
         })
 
+        // users order data post 
+        app.post('/users-order-data',async(req,res)=>{
+            const body=req.body.body
+            const result=await usersOrderCollection.insertOne(body)
+            res.send(result)
+        })
+        // users order data get 
+        app.get('/users-order-data',async(req,res)=>{
+           const email=req.query.email
+           const filter={email}
+           const query= usersOrderCollection.find(filter)
+           const result=await query.toArray()
+           res.send(result)
+        })
     } finally {
 
     }
