0370163ae6e36537b956e6ab6577c2678acbd51e
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-05-25 15:40:59 +0600

    order api completer

--- index.js
+++ index.js
@@ -3,12 +3,26 @@ const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 app.use(cors())
 app.use(express.json())
 require('dotenv').config()
 
-
-
+const verifyJwt = (req, res, next) => {
+    const authorization = req.headers.authorization
+    if (!authorization) {
+        return res.status(401).send({ message: 'unAuthorized access' })
+    }
+    const token = authorization.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden' })
+        }
+        console.log(decoded)
+        req.decoded = decoded
+        next()
+    })
+}
 
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.e98yk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -21,19 +35,25 @@ const run = async () => {
         const partsCollection = client.db('Bike_Parts').collection('parts')
         const reviewCollection = client.db('Bike_Parts').collection('review')
         const usersOrderCollection = client.db('Bike_Parts').collection('users_order')
+        const userCollection = client.db('Bike_Parts').collection('users')
         // get service data 
         app.get('/service-get', async (req, res) => {
             const result = await serviceCollection.find().toArray()
             res.send(result)
         })
         // get service by id 
-        app.get('/get-service/:id',async(req,res)=>{
-            const id=req.params.id
-            console.log(id)
-            const filter={_id:ObjectId(id)}
-            const result=await serviceCollection.findOne(filter)
-            res.send(result)
-
+        app.get('/get-service', verifyJwt, async (req, res) => {
+            const id = req.query.id
+            console.log('from ',req.query)
+            const email = req.query.email
+            if (req.decoded.email === email) {
+                const filter = { _id: ObjectId(id) }
+                const result = await serviceCollection.findOne(filter)
+                res.send(result)
+                console.log(result)
+            }else{                                                          
+                res.status(401).send({message:'Forbidden access'})
+            }                                                                                                                                                                   
         })
         // get helmet data 
         app.get('/get-helmet', async (req, res) => {
@@ -53,19 +73,44 @@ const run = async () => {
         })
 
         // users order data post 
-        app.post('/users-order-data',async(req,res)=>{
-            const body=req.body.body
-            const result=await usersOrderCollection.insertOne(body)
+        app.post('/users-order-data', async (req, res) => {
+            const body = req.body.body
+            const result = await usersOrderCollection.insertOne(body)
             res.send(result)
         })
         // users order data get 
-        app.get('/users-order-data',async(req,res)=>{
-           const email=req.query.email
-           const filter={email}
-           const query= usersOrderCollection.find(filter)
-           const result=await query.toArray()
-           res.send(result)
+        app.get('/users-order-data', verifyJwt,async (req, res) => {
+            const email = req.query.email
+            const filter = { email }
+            const query = usersOrderCollection.find(filter)
+            const result = await query.toArray()
+            res.send(result)
         })
+        // get user data for token 
+        app.get('/get-payment/:id',async(req,res)=>{
+            const id=req.params.id
+            const filter={_id:ObjectId(id)}
+            const result=await usersOrderCollection.findOne(filter)
+            res.send(result)
+            console.log(result)
+        })
+        app.put('/token', async (req, res) => {
+            const email = req.body.email
+            const user = req.body
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const accessToken = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+                expiresIn: '1d'
+            })
+            console.log(result)
+            res.send({ accessToken })
+        })
+
+
     } finally {
 
     }
