6c24fe543889963b278d1015fb5bbb2ee5b612ee
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-05-28 23:41:50 +0600

    complete

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 // const { get } = require('express/lib/response');
 app.use(cors({
-    origin:['http://localhost:3000','https://parts-mania.web.app']
+    origin:'https://parts-mania.web.app'
 }))
 app.use(express.json())
-const stripe = require('stripe')(process.env.STRIPE_TEST_KEY);
+const stripe = require('stripe')('sk_test_51L112oK2utpV7xig0jOmHp2eoSoktIhtmAJ1oCJtV3mM7yYI7w1e3NKQYKkPJqViX4Ihblcenmf1Aag3cye4Ln0o00AKqnzh4A');
 const verifyJwt = (req, res, next) => {
     const authorization = req.headers.authorization
     if (!authorization) {
